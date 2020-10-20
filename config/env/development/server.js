5ef05d8ad02247348b4beec3d9aad9a99640d7d2
Evgeniy Golubev <dev.egolubev@gmail.com>
2020-10-20 18:20:08 +0300

    Updates

--- config/env/development/server.js
+++ config/env/development/server.js
@@ -3,7 +3,7 @@ module.exports = ({ env }) => ({
   port: env.int("PORT", 1337),
   admin: {
     auth: {
-      secret: env("ADMIN_JWT_SECRET"),
+      secret: env("ADMIN_JWT_SECRET", "3c2e9f1cb6b41063f45854146d5d821d"),
     },
   },
 });
--- config/server.js
+++ config/server.js
@@ -1,4 +1,9 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  admin: {
+    auth: {
+      secret: env("ADMIN_JWT_SECRET", "3c2e9f1cb6b41063f45854146d5d821d"),
+    },
+  },
 });
